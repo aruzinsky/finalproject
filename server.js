00a591695935f201c8448274ffeddc19e212ce22
Michael Ross <michael.stewart.ross@gmail.com>
2015-05-12 13:34:40 -0700

    setting up koa server

---
+++ server.js
@@ -0,0 +1,26 @@
+var render = require('./render');
+var logger = require('koa-logger');
+var route = require('koa-route');
+var serve = require('koa-static');
+var koa = require('koa');
+var app = module.exports = koa();
+
+// middleware
+
+app.use(logger());
+
+// route middleware
+
+app.use(route.get('/', index));
+app.use(serve(__dirname + '/public'));
+
+// route definitions
+
+function *index() {
+	this.body = yield render('FinalProject-Home', {});
+}
+
+if (!module.parent) {
+	app.listen(3001);
+	console.log('Final Project is listening on port 3001');
+}
